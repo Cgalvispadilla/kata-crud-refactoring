e71e1b00c9e94a0622f1edd7e1d6a9f683a2eeca
Cgalvispadilla <fktcg99@gmail.com>
2021-09-16 01:09:17 -0500

    Se creo la funcionalidad de listar las listas de todos en el service

--- back/src/main/java/co/com/sofka/crud/services/InterfaceServiceGroupListTodo.java
+++ back/src/main/java/co/com/sofka/crud/services/InterfaceServiceGroupListTodo.java
@@ -2,8 +2,11 @@ package co.com.sofka.crud.services;
 
 import co.com.sofka.crud.dto.ListTodoDTO;
 
+import java.util.ArrayList;
+
+
 public interface InterfaceServiceGroupListTodo <T extends ListTodoDTO>{
-    public Iterable<T> list();
+    public ArrayList<T> list();
     public T save(T dto);
     public void delete(Long id);
     public T get(Long id);
--- back/src/main/java/co/com/sofka/crud/services/ListTodoService.java
+++ back/src/main/java/co/com/sofka/crud/services/ListTodoService.java
@@ -1,13 +1,29 @@
 package co.com.sofka.crud.services;
 
+import co.com.sofka.crud.converter.ListTodoConverter;
 import co.com.sofka.crud.dto.ListTodoDTO;
+import co.com.sofka.crud.dto.TodoDTO;
+import co.com.sofka.crud.entities.ListTodo;
+import co.com.sofka.crud.repositories.ListTodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ListTodoService implements InterfaceServiceTodo<ListTodoDTO>{
+
+    @Autowired
+    private ListTodoRepository repository;
+    @Autowired
+    private ListTodoConverter converter;
+
     @Override
-    public Iterable<ListTodoDTO> list() {
-        return null;
+    public ArrayList<ListTodoDTO> list() {
+        ArrayList<ListTodoDTO> listTodoDTOList = new ArrayList<>();
+        repository.findAll().forEach(listTodo -> listTodoDTOList.add(converter.convertToDto(listTodo)));
+        return listTodoDTOList;
     }
 
     @Override
